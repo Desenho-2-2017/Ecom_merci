b82eebcd947f28b512f7546b368c9163601df9a2
Gabriela Alves <gabrielaalvesdagama@gmail.com>
2017-09-03 22:45:58 -0300

    Creating view for test

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+# This code is just for testing only and will be deleted.
+class FrontView (View):
+    http_method_names = [u'get', u'post']
+
+    def get(self,request):
+        return render(request,'index.html')
