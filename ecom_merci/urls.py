8a2d10709382a6fd8dfdcee5546d0b5e83fb0f31
Gabriela Alves <gabrielaalvesdagama@gmail.com>
2017-09-03 22:47:35 -0300

    Configuring url from index page to test

--- ecom_merci/urls.py
+++ ecom_merci/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+import products.urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include(products.urls)),
 ]
