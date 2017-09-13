c4970a90414f8827f740286a12eaeb2163b96c8b
Vinícius Oliveira <vinifladf@gmail.com>
2017-09-12 22:05:46 -0300

    Criando form, url e views para excluir conta

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 # from django.contrib.auth.decorators import login_required
-from .views import (CustomerUserRegistrationView)
+from .views import (CustomerUserRegistrationView, CustomerUserDelectionView)
 
 
 urlpatterns = [
     url(r'^sign_up/$', CustomerUserRegistrationView.as_view(),
         name='sign_up'),
+    url(r'^excluir_conta/$', CustomerUserDelectionView.as_view(),
+        name='excluir_conta'),
 ]
