3773b74473e9f2ebdc5e98f4f606902e702c50a8
Renatafsouza <renata.rfsouza@gmail.com>
2017-09-25 13:43:45 -0300

    Removing Max unsused validator

--- products/models.py
+++ products/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MinValueValidator
 
 
 class ProductCategory(models.Model):
