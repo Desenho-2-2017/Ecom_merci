ffd1a751ff33b9bfaded2e49833320c7a8f3e2b8
IagoCarvalho <IagoVC_2012@hotmail.com>
2017-10-12 22:42:11 -0300

    Fixing cart not added in apps conf and adding migrations

--- ecom_merci/settings.py
+++ ecom_merci/settings.py
@@ -33,6 +33,7 @@ else:
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'products.apps.ProductsConfig',
+    'cart.apps.CartConfig',
     'sales.apps.SalesConfig',
     'django_extensions',
     'suit',
