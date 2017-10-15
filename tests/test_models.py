e4252019ce949279dbfbfd107646ff60bca48adc
Eliseu Egewarth <eliseuegewarth@gmail.com>
2017-10-15 19:10:21 -0200

    Fixing broken syntax
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,33 +1,57 @@
-# Remenber to use test_TESTNAME.py
-from users.models import *
 import pytest
+from users.models import (PhoneNumber, CustomerUser)
+
+# Remenber to use test_TESTNAME.py
+
 
 @pytest.mark.django_db
-def test_PhoneNumber():
+def test_phone_number():
 
     phone_number = PhoneNumber()
 
     phone_number.phone = '33576474'
 
     phone_number.save()
-    phone_numbers = PhoneNumber.objects.all().count()
+    count_phone_numbers = PhoneNumber.objects.all().count()
 
-    assert phone_numbers >= 1
-    phone_numbers.delete()
+    assert count_phone_numbers >= 1
+    phone_number.delete()
+
 
 @pytest.mark.django_db
-def test_CustomerUser():
+def test_customer_user():
+
+    phone_number = PhoneNumber()
+    phone_number.phone = '33576474'
+    phone_number.save()
+
+    phone_number2 = PhoneNumber()
+    phone_number2.phone = '12345678'
+    phone_number2.save()
+
+    phone_number3 = PhoneNumber()
+    phone_number3.phone = '87654321'
+    phone_number3.save()
 
     user = CustomerUser()
 
-    user.phone_numbers = '33576474'
     user.name = 'Gabriela'
     user.last_name = 'Gama'
     user.set_password('123456')
     user.email = 'gaby@mail.com'
     user.save()
+    user.phone_numbers.add(phone_number)
+    user.phone_numbers.add(phone_number2)
+    user.phone_numbers.add(phone_number3)
+    user.save()
 
-    users = CustomerUser.objects.all().count()
+    count_users = CustomerUser.objects.filter(pk=user.pk).count()
 
-    assert users >= 1
-    users.delete()
+    assert count_users >= 1
+    user.phone_numbers.remove(phone_number)
+    user.phone_numbers.remove(phone_number2)
+    user.phone_numbers.remove(phone_number3)
+    phone_number.delete()
+    phone_number2.delete()
+    phone_number3.delete()
+    user.delete()
