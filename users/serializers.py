adb9748d360406d4b06a2e73c1adcb62f4bbad2e
Mateus de Morais <mateusmorais@hotmail.com.br>
2017-10-30 15:28:41 -0200

    Improve users serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework.authtoken.models import Token
 from rest_framework import serializers
 from .models import (
     CustomerUser,
@@ -13,10 +14,34 @@ class PhoneNumberSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class CustomerUserSerializer(serializers.ModelSerializer):
+class CustomerUserSerializerDefault(serializers.ModelSerializer):
     class Meta:
         model = CustomerUser
         fields = '__all__'
+
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+
+class CustomerUserSerializerPOST(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerUser
+        fields = ('pk', 'username', 'first_name', 'last_name',
+                  'email', 'password')
+
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def create(self, validated_data):
+        user = CustomerUser(**validated_data)
+        password = validated_data['password']
+        user.set_password(password)
+        user.save()
+        token = Token.objects.create(user=user)
+        token.save()
+        return user
 
 
 class CreditCardSerializer(serializers.ModelSerializer):
