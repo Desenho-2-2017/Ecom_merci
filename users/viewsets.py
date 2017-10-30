c30eadd73972f1435b8ce46c389aad5a008f7ca0
João Henrique Egewarth <egewarth@gmail.com>
2017-10-30 09:54:42 -0200

    Criando viewsets de users

---
+++ users/viewsets.py
@@ -0,0 +1,23 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import (CustomerUser, PhoneNumber, CreditCard, ShippingAddress)
+from .serializers import (CustomerUserSerializer, PhoneNumberSerializer, CreditCardSerializer, ShippingAddressSerializer)
+
+
+class CustomerUserViewSet(ModelViewSet):
+    queryset = CustomerUser.objects.all()
+    serializer_class = CustomerUserSerializer
+
+
+class PhoneNumberViewSet(ModelViewSet):
+    queryset = PhoneNumber.objects.all()
+    serializer_class = PhoneNumberSerializer
+
+
+class CreditCardViewSet(ModelViewSet):
+    queryset = CreditCard.objects.all()
+    serializer_class = CreditCardSerializer
+
+
+class ShippingAddressViewSet(ModelViewSet):
+    queryset = ShippingAddress.objects.all()
+    serializer_class = ShippingAddressSerializer
