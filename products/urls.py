c6c0bcf9fff6689c28458f85c2243c97d2ebcd24
Author: Vinícius Oliveira <vinifladf@gmail.com>
2017-10-30 22:51:06 -0200

    Fix #23 Fixing issue Signed-off-by: Vinícius Oliveira <vinifladf@gmail.com>
    Rebuilding broked commit tree
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- products/urls.py
+++ products/urls.py
@@ -2,17 +2,20 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
-from products.views import (addProductToCartView,
-                            categoryDetailView,
-                            categoryIndexView,
-                            productDetailView,
-                            productIndexView)
+from products.views import (
+    productIndexView,
+    productDetailView,
+    categoryIndexView,
+    categoryDetailView,
+    addProductToCartView,
+    productFilterView,
+    )
 
 urlpatterns = [
     url(r'^index/', productIndexView, name='productIndexView'),
     url(r'^(?P<product_id>[0-9]+)/details/$', productDetailView,
         name='productDetailView'),
-
+    url(r'^productFilter/', productFilterView, name='productFilterView'),
     url(r'^list/', categoryIndexView, name='categoryIndexView'),
     url(r'^(?P<category_id>[0-9]+)/products_list/$', categoryDetailView,
         name='productDetailView'),
