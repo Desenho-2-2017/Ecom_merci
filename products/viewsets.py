bb0cb98f611229cc9b4814120249e2b7ce41e770
João Henrique Egewarth <egewarth@gmail.com>
2017-11-24 02:27:20 -0200

    Swagger Fazendo comentarios do swagger para classe de produtos.

--- products/viewsets.py
+++ products/viewsets.py
@@ -1,4 +1,3 @@
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from .models import (
     ProductCategory,
@@ -10,9 +9,9 @@ from .serializers import (
     ProductSerializerDefault,
     ProductSerializerPOST
     )
-# from .permissions import (
-#     ProductPermissions
-#     )
+from .permissions import (
+    ProductPermissions
+    )
 
 
 class ProductCategoryViewSet(ModelViewSet):
@@ -22,7 +21,7 @@ class ProductCategoryViewSet(ModelViewSet):
     """
     queryset = ProductCategory.objects.all()
     serializer_class = ProductCategorySerializerDefault
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (ProductPermissions,)
 
     def get_serializer_class(self):
         if self.action == 'create':
@@ -82,6 +81,7 @@ class ProductCategoryViewSet(ModelViewSet):
         """
         API endpoint that allows the return\
         of a product category through the method Get.
+        Receive the ID of product category.
         ---
         Response example:
         ```
@@ -137,47 +137,138 @@ class ProductViewSet(ModelViewSet):
     """
     queryset = Product.objects.all()
     serializer_class = ProductSerializerDefault
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (ProductPermissions,)
 
     def get_serializer_class(self):
-        """
-        API endpoint that allows category to be viewed.
-        """
         if self.action == 'create':
             return ProductSerializerPOST
         return ProductSerializerDefault
 
     def list(self, request):
         """
-        API endpoint that allows category to be viewed.
+        API endpoint that allows product to be viewed.
+        ---
+        Response example:
+        Return a list of:
+        ```
+        {
+            "id": "integer",
+            "product_name": "string",
+            "stock_quantity": "integer",
+            "price": "integer",
+            "weight": "integer",
+            "width": "integer",
+            "height": "integer",
+            "product_type": "string",
+            "illustration": "path",
+            "category_id": "integer"
+        }
+        ```
         """
         response = super(ProductViewSet, self).list(request)
         return response
 
     def create(self, request):
         """
-        API endpoint that allows category to be created.
+        API endpoint that allows product to be created.
+        Body example:
+        ```
+        {
+            "stock_quantity": "integer",
+            "product_name": "string",
+            "price": "integer",
+            "width": "integer",
+            "height": "integer",
+            "category_id": "string",
+            "product_type": "string",
+            "illustration": "string",
+            "weight": "integer"
+        }
+        ```
+        ---
+        Response example:
+        ```
+        {
+            "id": "integer",
+            "product_name": "string",
+            "stock_quantity": "integer",
+            "price": "integer",
+            "weight": "integer",
+            "width": "integer",
+            "height": "integer",
+            "product_type": "string",
+            "illustration": "path",
+            "category_id": "integer"
+        }
+        ```
         """
         response = super(ProductViewSet, self).create(request)
         return response
 
     def destroy(self, request, pk=None):
         """
-        API endpoint that allows category to be deleted.
+        API endpoint that allows product to be deleted.
         """
         response = super(ProductViewSet, self).destroy(request, pk)
         return response
 
     def retrieve(self, request, pk=None):
         """
-        API endpoint that allows category to be viewed.
+        API endpoint that allows allow the return\
+        of a product through the method Get.
+        Receive the ID of product.
+        ---
+        Receive
+        Response example:
+        ```
+        {
+            "id": "integer",
+            "product_name": "string",
+            "stock_quantity": "integer",
+            "price": "integer",
+            "weight": "integer",
+            "width": "integer",
+            "height": "integer",
+            "product_type": "string",
+            "illustration": "path",
+            "category_id": "integer"
+        }
+        ```
         """
         response = super(ProductViewSet, self).retrieve(request, pk)
         return response
 
     def partial_update(self, request, pk=None, **kwargs):
         """
-        API endpoint that allows category to be updated.
+        API endpoint that allows product to be updated.
+        ---
+        Parameters:
+        Cart ID and a JSON with one or more attributes of product.
+        Full updade:
+        ```
+        {
+            "stock_quantity": "integer",
+            "product_name": "string",
+            "price": "integer",
+            "width": "integer",
+            "height": "integer",
+            "category_id": "string",
+            "product_type": "string",
+            "illustration": "string",
+            "weight": "integer"
+        }
+        ```
+        ---
+        Partial updade:
+        ```
+        {
+            "stock_quantity": "integer",
+            "product_name": "string",
+            "price": "integer",
+            "illustration": "string",
+            "weight": "integer"
+        }
+        ```
         """
         response = super(ProductViewSet, self).\
             partial_update(request, pk, **kwargs)
@@ -186,6 +277,18 @@ class ProductViewSet(ModelViewSet):
     def update(self, request, pk=None, **kwargs):
         """
         API endpoint that allows category to be updated.
+        ---
+        Parameters:
+        Product ID and a JSON with all required attributes.
+        ```
+        {
+            "stock_quantity": "integer",
+            "product_name": "string",
+            "price": "integer",
+            "category_id": "string",
+            "illustration": "string"
+        }
+        ```
         """
         response = super(ProductViewSet, self).update(
             request, pk, **kwargs
